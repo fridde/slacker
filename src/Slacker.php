4bf603746ebca2a7e95a8d654934f842ef48cc02
fridde <f@hehl.se>
2018-08-26 23:12:12 +0200

    finally more usability

--- src/Slacker.php
+++ src/Slacker.php
@@ -6,5 +6,18 @@ use Maknz\Slack\Client;
 
 class Slacker extends Client
 {
+    private $settings;
+    private $webhook_url;
 
+    /**
+     * Slacker constructor.
+     */
+    public function __construct(array $settings)
+    {
+        $this->settings['username'] = $settings['username'];
+        $this->settings['channel'] = $settings['channel'];
+        $this->webhook_url = $settings['webhook_url'];
+
+        parent::__construct($this->webhook_url, $this->settings);
+    }
 }
